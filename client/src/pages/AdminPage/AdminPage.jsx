7cebb1a9e283d28dbbf3c0264a46f0b69010de9c
AntonyPry <06032004anton@gmail.com>
2025-03-24 07:51:06 +0300

    admin page

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,6 @@
 // pages/AdminPage/AdminPage.jsx
 import React from 'react';
-import { Divider, Statistic } from 'antd';
+import { Card, Divider, Statistic } from 'antd';
 import styles from './AdminPage.module.css';
 
 const AdminPage = () => {
@@ -10,7 +10,7 @@ const AdminPage = () => {
         {/* Левая колонка: карточки 1 и 3 */}
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '32px' }}>
           {/* Карточка 1 */}
-          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255,216,216)', position: 'relative' }}>
+          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255, 255, 255)', position: 'relative' }}>
             <h3 className={styles.cardName}>Продай еще, чтобы выполнить план:</h3>
             {/* Сетка на 2 столбца */}
             <div
@@ -33,12 +33,12 @@ const AdminPage = () => {
           </div>
 
           {/* Карточка 3 */}
-          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255,216,216)', position: 'relative' }}>
+          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255, 255, 255)', position: 'relative' }}>
             <h3 className={styles.cardName}>Статистика</h3>
             <div style={{ marginTop: 16 }}>
               {/* Раздел "Выручка" */}
               <div>
-                <h4 style={{ marginBottom: '8px' }}>Выручка:</h4>
+                <h4 style={{ margin: 0, marginBottom: '8px' }}>Выручка:</h4>
                 <div style={{ display: 'flex', gap: '16px' }}>
                   <div style={{ flex: '0 0 33%' }}>
                     <Statistic title="Факт" value={20000} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
@@ -51,6 +51,8 @@ const AdminPage = () => {
                   </div>
                 </div>
               </div>
+
+              <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
 
               {/* Раздел "Еда" */}
               <div>
@@ -70,49 +72,91 @@ const AdminPage = () => {
 
               <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
 
-              {/* После разделителя: фиксированные 3 столбца */}
-              <div
-                style={{
-                  display: 'grid',
-                  gridTemplateColumns: 'repeat(3, 1fr)',
-                  gap: '16px',
-                }}
-              >
-                <Statistic title="Напитки (Факт)" value={3000} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="Напитки (План)" value={3500} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic
-                  title="Напитки (Выполнение)"
-                  value="86%"
-                  valueStyle={{ fontSize: '18px', fontWeight: 'bold' }}
-                />
-                <Statistic title="Шоколад (Факт)" value={1500} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="Шоколад (План)" value={1800} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic
-                  title="Шоколад (Выполнение)"
-                  value="83%"
-                  valueStyle={{ fontSize: '18px', fontWeight: 'bold' }}
-                />
-                <Statistic
-                  title="Френч-доги и сендвичи (Факт)"
-                  value={2200}
-                  valueStyle={{ fontSize: '18px', fontWeight: 'bold' }}
-                />
-                <Statistic
-                  title="Френч-доги и сендвичи (План)"
-                  value={2500}
-                  valueStyle={{ fontSize: '18px', fontWeight: 'bold' }}
-                />
-                <Statistic
-                  title="Френч-доги и сендвичи (Выполнение)"
-                  value="88%"
-                  valueStyle={{ fontSize: '18px', fontWeight: 'bold' }}
-                />
-                <Statistic title="PS (Факт)" value={4000} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="PS (План)" value={4500} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="PS (Выполнение)" value="89%" valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="ПК (Факт)" value={3200} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="ПК (План)" value={3500} valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
-                <Statistic title="ПК (Выполнение)" value="91%" valueStyle={{ fontSize: '18px', fontWeight: 'bold' }} />
+              {/* Раздел "Напитки" */}
+              <div>
+                <h4 style={{ marginBottom: '8px' }}>Напитки:</h4>
+                <div style={{ display: 'flex', gap: '16px' }}>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Факт" value={3500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="План" value={1500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Выполнение" value="86%" valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                </div>
+              </div>
+
+              <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
+
+              {/* Раздел "Шоколад" */}
+              <div>
+                <h4 style={{ marginBottom: '8px' }}>Шоколад:</h4>
+                <div style={{ display: 'flex', gap: '16px' }}>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Факт" value={3500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="План" value={1500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Выполнение" value="86%" valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                </div>
+              </div>
+
+              {/* Раздел "Френч-доги и сендвичи" */}
+              <div>
+                <h4 style={{ marginBottom: '8px' }}>Френч-доги и сендвичи:</h4>
+                <div style={{ display: 'flex', gap: '16px' }}>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Факт" value={3500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="План" value={1500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Выполнение" value="86%" valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                </div>
+              </div>
+
+              {/* Раздел "PS5" */}
+              <div>
+                <h4 style={{ marginBottom: '8px' }}>PS5:</h4>
+                <div style={{ display: 'flex', gap: '16px' }}>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Факт" value={3500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="План" value={1500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Выполнение" value="86%" valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                </div>
+              </div>
+
+              {/* Раздел "ПК" */}
+              <div>
+                <h4 style={{ marginBottom: '8px' }}>ПК:</h4>
+                <div style={{ display: 'flex', gap: '16px' }}>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic
+                      title="Факт"
+                      value={3500}
+                      titleStyle={{ margin: 0 }}
+                      valueStyle={{ fontSize: '20px', fontWeight: 'bold' }}
+                    />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="План" value={1500} valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                  <div style={{ flex: '0 0 33%' }}>
+                    <Statistic title="Выполнение" value="86%" valueStyle={{ fontSize: '20px', fontWeight: 'bold' }} />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -121,21 +165,25 @@ const AdminPage = () => {
         {/* Правая колонка: карточки 2 и 4 */}
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '32px' }}>
           {/* Карточка 2 */}
-          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255,216,216)', position: 'relative' }}>
+          <div className={styles.cardContent} style={{ backgroundColor: 'rgb(255, 255, 255)', position: 'relative' }}>
             <h3 className={styles.cardName}>Текущая премия:</h3>
-            <div style={{ marginTop: 16, display: 'flex', flexDirection: 'column', gap: '16px' }}>
+            <div style={{ marginTop: 16, display: 'flex', flexDirection: 'column' }}>
               <Statistic title="База" value={1000} valueStyle={{ fontSize: '24px', fontWeight: 'bold' }} />
+              <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
               <Statistic
                 title="За выполнение плана"
                 value={500}
                 valueStyle={{ fontSize: '24px', fontWeight: 'bold' }}
               />
+              <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
               <Statistic
                 title="За выполнение плана по еде"
                 value={300}
                 valueStyle={{ fontSize: '24px', fontWeight: 'bold' }}
               />
+
               <Divider style={{ backgroundColor: '#ccc', margin: '12px 0' }} />
+
               <Statistic title="Суммарно" value={1800} valueStyle={{ fontSize: '26px', fontWeight: 'bold' }} />
             </div>
           </div>
@@ -144,7 +192,7 @@ const AdminPage = () => {
           <div
             className={styles.cardContent}
             style={{
-              backgroundColor: 'rgb(255,216,216)',
+              backgroundColor: 'rgb(255, 255, 255)',
               position: 'relative',
               minHeight: '300px',
             }}
